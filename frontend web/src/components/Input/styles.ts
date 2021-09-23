2aa1a66385e7a6ff3eebb37a9735fa5443096708
Aristeu-dev <aristeugarcia.dev@gmail.com>
2021-09-23 18:10:02 -0300

    feat:add_unform

--- frontend web/src/components/Input/styles.ts
+++ frontend web/src/components/Input/styles.ts
@@ -1,5 +1,11 @@
-import styled from "styled-components";
-export const Container = styled.div`
+import styled, { css } from "styled-components";
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   border-radius: 10px;
   border: 1px solid #e2e2e8;
   padding: 16px;
@@ -10,6 +16,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: blue;
+      border-color: blue;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+     color: blue;
+      /* border-color: blue; */
+    `}
   input {
     flex: 1;
     border: 0;
--- frontend web/src/pages/SignUp/index.tsx
+++ frontend web/src/pages/SignUp/index.tsx
@@ -4,32 +4,38 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
 import logo from "../../assets/logo.png";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { Form } from "@unform/web";
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logo} alt="GoBarber" />
-      <form>
-        <h1>Faça o seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: Object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit">Cadastrar</Button>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
 
-        <a href="">
-          <FiArrowLeft />
-          Voltar
-        </a>
-      </form>
-    </Content>
-  </Container>
-);
+          <a href="">
+            <FiArrowLeft />
+            Voltar
+          </a>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
