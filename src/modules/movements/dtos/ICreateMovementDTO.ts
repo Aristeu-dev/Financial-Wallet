a40e52e01a2b1f07f8f48bf33d3d3ba26e0f9a3f
Aristeu Garcia <76592155+Aristeu-dev@users.noreply.github.com>
2021-11-16 12:55:29 -0300

    Delete src directory

--- src/modules/movements/dtos/ICreateMovementDTO.ts
+++
@@ -1,5 +0,0 @@
-export default interface ICreateMovementDTO {
-    id_category: String,
-    value: Number,
-    description: String
-}
--- src/modules/movements/repositories/IMovementsRepository.ts
+++
@@ -1,7 +0,0 @@
-import Movement from '../infra/typeorm/entities/Movement';
-import ICreateMovementDTO from '../dtos/ICreateMovementDTO';
-
-export default interface IMovementsRepository {
-    create(id_category:ICreateMovementDTO, value: ICreateMovementDTO, description: ICreateMovementDTO): Promise<Movement>;
-    findMovementByDate(date: Date): Promise<Movement[] | undefined>;
-}
